fe8f30c7ee7dbfc001a8fd26934461fd2eaeead6
mumaralfajar <kuhaaku@gmail.com>
2022-06-25 06:33:34 +0700

    initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etCity;
+    TextView tvResult;
+    private final String url = "";
+    private final String key = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getWeatherDetail(View view) {
+    }
+}
