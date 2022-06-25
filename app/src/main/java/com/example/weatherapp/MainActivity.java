67ff4ff0cce828e1b3397e17284b8589336c6ef9
mumaralfajar <kuhaaku@gmail.com>
2022-06-25 09:21:33 +0700

    final

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,23 +2,100 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     EditText etCity;
     TextView tvResult;
-    private final String url = "";
-    private final String key = "";
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String key = "4edb1d824158d3bbbe8c6a5c1a447e16";
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etCity = findViewById(R.id.etCity);
+        tvResult = findViewById(R.id.tvResult);
     }
 
     public void getWeatherDetail(View view) {
+        String tmpUrl = "";
+        String city = etCity.getText().toString().trim();
+
+        if (city.equals("")) {
+            tvResult.setText("Nama kota tidak boleh kosong!");
+        } else {
+            tmpUrl = url + "?q=" + city + "&appid=" + key;
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tmpUrl, new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    //Log.d("response", response);
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String cityName = jsonResponse.getString("name");
+                        tvResult.setTextColor(Color.rgb(68,134,199));
+                        output += "Cuaca Bandung saat ini  " + cityName + " (" + countryName + ")"
+                                + "\nTemp: " + df.format(temp) + " °C"
+                                + "\nFeels Like: " + df.format(feelsLike) + " °C"
+                                + "\nHumidity: " + humidity + "%"
+                                + "\nDescription: " + description
+                                + "\nWind Speed: " + wind + "m/s (meters per second)"
+                                + "\nCloudiness: " + clouds + "%"
+                                + "\nPressure: " + pressure + " hPa";
+                        tvResult.setText(output);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
     }
 }
